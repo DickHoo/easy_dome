72a6b4af484ff7fd5774c95d1c15ac11c14ae4e3
David Hoo <zhaihao@xwetc.cn>
2022-04-26 15:30:22 +0800

    [新增]padding margin 扩展

--- easy-ui/lib/easy_ui.dart
+++ easy-ui/lib/easy_ui.dart
@@ -1,5 +1,8 @@
 library easy_ui;
 export 'ui/widget/text/text.dart';
+export 'ui/widget/field/text_field.dart';
+export 'ui/layout/padding/padding.dart';
+export 'ui/layout/margin/margin.dart';
 import 'package:flutter/cupertino.dart';
 class UI {
   Widget operator >(Widget widget) => widget;
